43c70e504dd21a67c4a322dc58f1e1c268c2790d
Ajeesh25353646 <118684371+Ajeesh25353646@users.noreply.github.com>
2022-12-20 16:03:28 +0530

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,60 @@
+print("hello world",7)
+print(5**4)
+print("bye")
+print("Im a good boy\nIm 21")
+print("hey", 6, 7, sep="-", end="009\n")
+print("herry")
+b = complex(8, 2)
+print(b)
+print("the type of b is ",type(b))
+dict1 = {"name":"sakshi","age":20}
+print(dict1)
+name = "Ajeesh"
+print(name[0])
+me = """I am ajeesh
+I am not feeling well today"""
+print(me)
+a = 5
+b = 6
+#addition
+print(a + b)
+#multiplication
+print(a*b)
+#exponenetial
+print(a**b)
+#division
+print(a/b)
+#substraction
+print(a-b)
+name = "Ajeesh garg"
+print(name.find("j"))
+print(name.find("garg"))
+
+print(name.upper())
+print(name.replace("Ajeesh garg" , "tony stark"))
+
+#finding if the character is present in a string (True/False)
+print("garg" in name)
+print("h" in name)
+
+# writing character present at a specific index
+print(name[0])
+print(name[7])
+ 
+# Taking a new bigger string
+names = "Harry,Shubham"
+
+# For printing first five characters of the string
+print(names[0:5])
+
+# To find the lenght if a string
+print(len(names))
+
+#tells remainder
+print(5%2)
+
+
+
+
+
+
